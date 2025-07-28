1bcf9207ee804fcc1058fc0f17dd5de863100f3e
CrisMau1704 <cristian76724599@gmail.com>
2025-07-28 18:18:17 -0400

    Agrego rutas todas

--- config/cors.php
+++ config/cors.php
@@ -6,7 +6,11 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173'],  // El origen de tu frontend en Vue.js
+    'allowed_origins' => [
+    'http://localhost:5173',  // Desarrollo local
+    'https://zingy-brioche-36c7f0.netlify.app',  // Reemplaza con tu URL real de Netlify
+    'https://restaurant-back-z4n3.onrender.com'  // Tu backend en Render
+],  // El origen de tu frontend en Vue.js
 
     'allowed_origins_patterns' => [],
 
